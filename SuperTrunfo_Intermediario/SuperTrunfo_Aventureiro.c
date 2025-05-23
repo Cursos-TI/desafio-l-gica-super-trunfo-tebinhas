34266b83e06caa669d57034e78714503fb2548d4
tebinhas <mateus_tebas02@outlook.com>
2025-05-23 19:10:48 +0000

    feat: menu do jogo, menu de seleção de atributo, comparação de atributo  seleconado.

--- SuperTrunfo_Intermediario/SuperTrunfo_Aventureiro.c
+++ SuperTrunfo_Intermediario/SuperTrunfo_Aventureiro.c
@@ -10,14 +10,32 @@ int main(){
 -Exibicao de resultados
 */
 
+//Atributos das cartas
 char cidade1[20], cidade2[20], estado1[20], estado2[20], codigocarta1[5], codigocarta2[5];
-int pontosturisticos1, pontosturisticos2;
+int pontosturisticos1, pontosturisticos2, menujogo, menucomparacao;
 unsigned long int populacao1, populacao2;
 double pib1, pib2;
 float area1, area2, pibpercapta1, pibpercapta2, populacaoporarea1, populacaoporarea2;
 
 //Cadastro Carta 1
 printf ("##SuperTrunfo Mestre!##\n");
+
+//Menu Do jogo
+printf("1.Iniciar Jogo\n");
+printf("2.Regras\n");
+printf("3.Sair do Jogo\n");
+printf("Selecione a opção: ");
+scanf("%d",&menujogo);
+
+switch (menujogo)
+{
+case 1:
+    printf("Iniciando...\n");
+    break;
+default:
+    printf("Saindo...\n");
+    return 0;
+}
 
 //Declaração Atributos Carta 1
 printf ("\n##Dados da Carta 1##\n");
@@ -109,16 +127,50 @@ printf ("PIB: %.0f\n", pib2);
 printf ("População por área: %.0f\n", populacaoporarea2);
 printf ("PIB per capta: %.0f\n", pibpercapta2);
 
+//Menu Seleção de atributo
+printf("Selecione o atributo:\n");
+printf("1.População\n");
+printf("2.Pontos Turisticos\n");
+printf("3.Area\n");
+printf("4.PIB\n");
+printf("5.População por Area\n");
+printf("6.Pib per capta\n");
+scanf("%d",&menucomparacao);
 
+switch (menucomparacao)
+{
+case 1:
+printf("\nPopulação Selecionado\n");
+    break;
+case 2:
+printf("Pontos Turisticos Selecionado\n");
+    break;
+case 3:
+printf("Area Selecionado\n");
+    break;
+case 4:
+printf("PIB Selecionado\n");
+    break;
+case 5:
+printf("População por Area Selecionado\n");
+    break;
+default:
+printf("PIB per capta Selecionado\n");
+    break;
+}
 
-
-
-
-
-
-
-
-
+//Comparação do atributo selecionado
+if ((menucomparacao == 1 && populacao1 > populacao2) ||
+    (menucomparacao == 2 && pontosturisticos1 > pontosturisticos2) ||
+    (menucomparacao == 3 && area1 > area2) ||
+    (menucomparacao == 4 && pib1 > pib2) ||
+    (menucomparacao == 5 && populacaoporarea1 < populacaoporarea2) ||
+    (menucomparacao == 6 && pibpercapta1 < pibpercapta2)) {
+    
+printf("Carta %s Ganhou!\n",&codigocarta1);
+} else {
+printf("Carta %s Ganhou!\n",&codigocarta2);
+}
 
 
 
